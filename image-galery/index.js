eb68aa4f20887e6ffc66e32d39900caa678d40dc
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-03-10 23:47:01 +0200

    feat: add event listener to all imgs

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,9 +1,30 @@
-let url = 'https://api.unsplash.com/search/photos?query=random&per_page=30&orientation=landscape&client_id=2H0jtKmrZwpjrcnn9F5U0OJGu7MLmzBNaMMxf5zFZ1Y'
-
-//change url according to request
+let url = 'https://api.unsplash.com/search/photos?query=random&per_page=30&orientation=landscape&client_id=2H0jtKmrZwpjrcnn9F5U0OJGu7MLmzBNaMMxf5zFZ1Y';
 const topic = document.querySelector('.search-field');
 const content = document.querySelector('.gallery-container');
 const searchImage = document.querySelector('.search-icon');
+
+getData();
+
+// Fetch data
+async function getData() {
+    const res = await fetch(url);
+    const data = await res.json();
+    showData(data);
+}
+
+// Show images on the page
+function showData(data) {
+    for (let i=0; i<data.results.length; i++) {
+        const img = document.createElement('img');
+        img.classList.add('gallery-img');
+        img.src = data.results[i].urls.regular;
+        img.alt = data.results[i].alt_description;
+        img.addEventListener('click', () => ale(data.results[i].alt_description))
+        content.append(img);
+    }
+}
+
+//change url according to request
 function chooseTopic() {
     if (topic.value !== '') 
     content.innerHTML = '';
@@ -12,35 +33,18 @@ function chooseTopic() {
     getData();
 }
 
-// realize search after pushing Enter
+// search after pushing Enter
 document.addEventListener('keyup', theKey => {
     if (theKey.code === 'Enter' || theKey.code === 'NumpadEnter') chooseTopic()
 });
 
-// realize search after magnifier click
-searchImage.addEventListener('click', chooseTopic)
+// search after magnifier click
+searchImage.addEventListener('click', chooseTopic);
 
-//that main API function
-async function getData() {
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data);
 
-    showData(data);
+function ale(message) {
+    alert(message);
 }
-  getData();
-
-  //Show images on the page
-  function showData(data) {
-    for (let i = 0; i<data.results.length; i++) {
-        const img = document.createElement('img');
-        img.classList.add('gallery-img');
-        let pageImaes = data.results.map((el) => {return el.urls.regular})
-        img.src = `${pageImaes[i]}`;
-        img.alt = 'image';
-        galleyContainer.append(img);
-    }
-  }
 
 
     //Create image in the container - WORKS
